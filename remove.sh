0f9a4c441b783745efc4eb7fbd7f61834e2cd79d
tolaoguntunde <50242126+tolaoguntunde@users.noreply.github.com>
2022-10-08 14:48:20 -0400

    Update and rename remove to remove.sh

---
+++ remove.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#testing webhook with this code
+echo "Hello World to the world of Jenkins"
+curl -i ifconfig.co
+
+#!/bin/bash
+echo "running apt update"
+sudo apt update
+
+echo "installing nodejs"
+sudo apt install nodejs -y
+
+echo "installing NPM"
+sudo apt install npm -y
+
+echo "installing yarn"
+sudo npm install -g yarn -y
+
+echo "installing pm2"
+sudo npm install pm2 -g
+
+echo "installing project"
+npm install -y
+
+echo "Building project"
+yarn build
+
+echo "changing directory"
+cd dist
+
+echo "starting project in directory"
+pm2 start main.js
+
+echo "list running apps"
+pm2 list
+
+echo "Application deployed successfully"
+
+
+
+
